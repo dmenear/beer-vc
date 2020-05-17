47ffa9f9589953038e3d0e502e3b01d0a953e593
David Menear <dmenear@users.noreply.github.com>
2020-05-17 12:35:42 -0400

    Initialize Spring Boot application

---
+++ src/main/java/com/beervc/BeerVcApplication.java
@@ -0,0 +1,13 @@
+package com.beervc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerVcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerVcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beervc/BeerVcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beervc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerVcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
